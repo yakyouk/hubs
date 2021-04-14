d0e757ca49598e397a8e256a406401cd29e67e1f
Manuel Martin <manmartin@mozilla.com>
2021-04-14 23:48:54 +0200

    Lazy load media on scene entry

--- src/hub.js
+++ src/hub.js
@@ -272,6 +272,7 @@ function setupLobbyCamera() {
 
 let uiProps = {};
 let connectionErrorTimeout = null;
+const lazyLoadSceneMedia = configs.feature("lazy_load_media");
 
 // Hub ID and slug are the basename
 let routerBaseName = document.location.pathname
@@ -402,6 +403,7 @@ async function updateEnvironmentForHub(hub, entryManager) {
     environmentEl.addEventListener("model-error", sceneErrorHandler, { once: true });
 
     environmentEl.setAttribute("gltf-model-plus", { src: sceneUrl, useCache: false, inflate: true });
+    lazyLoadSceneMedia && sceneEl.emit("environment-scene-loading");
     environmentScene.appendChild(environmentEl);
   } else {
     // Change environment
@@ -440,6 +442,7 @@ async function updateEnvironmentForHub(hub, entryManager) {
           environmentEl.setAttribute("gltf-model-plus", { src: "" });
         }
         environmentEl.setAttribute("gltf-model-plus", { src: sceneUrl });
+        lazyLoadSceneMedia && sceneEl.emit("environment-scene-loading");
       },
       { once: true }
     );
@@ -453,6 +456,7 @@ async function updateEnvironmentForHub(hub, entryManager) {
       environmentEl.setAttribute("gltf-model-plus", { src: "" });
     }
     environmentEl.setAttribute("gltf-model-plus", { src: loadingEnvironment });
+    lazyLoadSceneMedia && sceneEl.emit("environment-scene-loading");
   }
 }
 
